652ad59ad9846e50623bf05652caa7d1d6c15d8c
etienne-chrp <66628513+etienne-chrp@users.noreply.github.com>
2021-03-16 18:57:45 +0100

    Add base Selenium ChromeWebdriver

---
+++ micro-cms-v2/Program.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace micro_cms_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var webDriver = LaunchBrowser();
+            try
+            {
+                // This is where we can start interacting with the browser
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while executing automation");
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                webDriver.Quit();
+            }
+        }
+
+        static IWebDriver LaunchBrowser()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+            options.AddArgument("--disable-notifications");
+
+            var driver = new ChromeDriver(options);
+            return driver;
+        }
+    }
+}
